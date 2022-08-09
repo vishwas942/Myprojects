2a7a0d6a57c675a168d7cb3fab6239d198dd9608
vishwas942 <106774520+vishwas942@users.noreply.github.com>
2022-08-09 20:09:32 +0530

    Add files via upload

---
+++ Expandingcards/expandingjs.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+    panel.addEventListener('click', ()=> {
+        removeActiveclass();
+        panel.classList.add('active');
+    })
+})
+
+function removeActiveclass(){
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+}
+
+
